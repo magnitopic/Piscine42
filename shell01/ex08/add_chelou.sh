65aa99859c1d44a163aa0fb5dafb3fcf8cdaef60
magnitopic <magnitopic@gmail.com>
2022-07-07 21:21:11 +0200

    08 almoust finished

--- shell01/ex08/add_chelou.sh
+++ shell01/ex08/add_chelou.sh
@@ -1,1 +1,5 @@
 #!/bin/sh
+# FT_NBR1 no funciona bien, \ lo traduce como 10
+FT_NBR1=$(echo "\\\'?\"\\\"\'\\" | tr "\'\\\\\"?!" "01234")
+FT_NBR2=$(echo "rcrdmddd" | tr "mrdoc" "01234")
+echo $(($FT_NBR1+$FT_NBR2)) | tr "012345678910111213" "gtaio luSnemf"
