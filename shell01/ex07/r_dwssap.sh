5bc2e5e133517b449dc7c5015a315cc7c4cdb8fa
magnitopic <magnitopic@gmail.com>
2022-07-07 13:26:24 +0200

    07 terminado

--- shell01/ex07/r_dwssap.sh
+++ shell01/ex07/r_dwssap.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cat /etc/passwd | tail -n+11 | awk 'NR % 2 == 0' | rev |sort -r | cut -d ":" -f 7
+cat /etc/passwd | grep -v '#' | awk 'NR%2==0'| awk -F ":" '{print " ,"$1}' | rev | sort -r | awk 'NR>7{print $0}' | awk 'NR<15{print $0}' | tr -d '\n' | sed "s/, *$/./"
