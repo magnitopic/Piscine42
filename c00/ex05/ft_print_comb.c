0881d0a0a05e7872f872a3db06bffbc073a93a94
magnitopic <magnitopic@gmail.com>
2022-07-11 15:24:32 +0200

    ex 05 terminado

--- c00/ex05/ft_print_comb.c
+++ c00/ex05/ft_print_comb.c
@@ -12,11 +12,20 @@
 
 #include <unistd.h>
 
-int	main(void)
+void	putchar(char i, char j, char k)
 {
-	char i;
-	char j;
-	char k;
+	write(1, &i, 1);
+	write(1, &j, 1);
+	write(1, &k, 1);
+	if (i < '7' || j < '8' || k < '9')
+		write(1, ", ", 2);
+}
+
+void	ft_print_comb(void)
+{
+	char	i;
+	char	j;
+	char	k;
 
 	i = '0';
 	j = '1';
@@ -29,16 +38,11 @@ int	main(void)
 			k = j +1;
 			while (k <= '9')
 			{
-				write(1, &i, 1);
-				write(1, &j, 1);
-				write(1, &k, 1);
-				if (i != 7 && j != 8 && k != 9)
-					write(1, ", ", 2);
+				putchar(i, j, k);
 				k++;
 			}
 			j++;
 		}
 		i++;
 	}
-	return 0;
 }
