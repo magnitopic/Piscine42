e4c7122bd3ef8766f64971e9543741fa76e43464
magnitopic <magnitopic@gmail.com>
2022-07-14 19:09:51 +0200

    added commented main

--- c01/ex01/ft_ultimate_ft.c
+++ c01/ex01/ft_ultimate_ft.c
@@ -6,11 +6,39 @@
 /*   By: alaparic <alaparic@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 21:20:09 by alaparic          #+#    #+#             */
-/*   Updated: 2022/07/13 21:20:11 by alaparic         ###   ########.fr       */
+/*   Updated: 2022/07/14 17:20:06 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 void	ft_ultimate_ft(int *********nbr)
 {
-	nbr = 42;
+	*********nbr = 42;
 }
+
+/*int	main(void)
+{
+	int nbr;
+	int *nbr1;
+	int **nbr2;
+	int ***nbr3;
+	int ****nbr4;
+	int *****nbr5;
+	int ******nbr6;
+	int *******nbr7;
+	int ********nbr8;
+
+	nbr=0;
+	nbr1 = &nbr;
+	nbr2 = &nbr1;
+	nbr3 = &nbr2;
+	nbr4 = &nbr3;
+	nbr5 = &nbr4;
+	nbr6 = &nbr5;
+	nbr7 = &nbr6;
+	nbr8 = &nbr7;
+	ft_ultimate_ft(&nbr8);
+	printf("%i", nbr);
+	return (0);
+}*/
