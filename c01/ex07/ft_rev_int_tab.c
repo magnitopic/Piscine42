048d71e08c493484af9d83db5fca97fb916ae465
magnitopic <magnitopic@gmail.com>
2022-07-15 12:57:38 +0200

    finished ex07

--- c01/ex07/ft_rev_int_tab.c
+++ c01/ex07/ft_rev_int_tab.c
@@ -1,21 +1,48 @@
-void ft_rev_int_tab(int *tab, int size)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alaparic <alaparic@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/15 11:04:59 by alaparic          #+#    #+#             */
+/*   Updated: 2022/07/15 12:43:07 by alaparic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_rev_int_tab(int *tab, int size)
 {
-	int i;
-	int *revArray;
+	int	i;
+	int	*rev_array;
 
 	i = 0;
-	while (i <= size)
+	while (i <= size - 1)
 	{
-		revArray[i] = tab[i - size - 1];
+		rev_array[i] = tab[(size - 1) - i];
 		i++;
 	}
-	tab = revArray;
+	i = 0;
+	while (i <= size - 1)
+	{
+		tab[i] = rev_array[i];
+		i++;
+	}
 }
 
-int main(void)
+/*int	main(void)
 {
-	int size = 4;
-	int tab[4] = {0, 1, 1, 6};
+	int	i = 0;
+	int	size = 4;
+	int	tab[4] = {0, 1, 1, 6};
+
 	ft_rev_int_tab(tab, size);
+	while (i <= size - 1)
+	{
+		printf("%d", tab[i]);
+		i++;
+	}
 	return (0);
-}
+}*/
