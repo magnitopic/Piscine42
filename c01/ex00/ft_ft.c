7e76c397541aeb35c6590297ae42160800ae42d9
magnitopic <magnitopic@gmail.com>
2022-07-15 12:55:51 +0200

    main for ex00

--- c01/ex00/ft_ft.c
+++ c01/ex00/ft_ft.c
@@ -10,7 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <stdio.h>
+
 void	ft_ft(int *nbr)
 {
-	nbr = 42;
+	*nbr = 42;
 }
+
+/*int	main(void)
+{
+	int	nbr;
+
+	nbr = 0;
+	ft_ft(&nbr);
+	printf("%d", nbr);
+	return (0);
+}*/
