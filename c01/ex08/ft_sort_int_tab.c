e04b33d4765e88272e5f2d6c071a8693531cdf67
magnitopic <magnitopic@gmail.com>
2022-07-16 20:52:28 +0200

    mi mierda ex08

--- c01/ex08/ft_sort_int_tab.c
+++ c01/ex08/ft_sort_int_tab.c
@@ -6,27 +6,42 @@
 /*   By: alaparic <alaparic@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 12:51:38 by alaparic          #+#    #+#             */
-/*   Updated: 2022/07/15 17:20:41 by alaparic         ###   ########.fr       */
+/*   Updated: 2022/07/16 20:51:20 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
-void 
-
-void ft_sort_int_tab(int *tab, int size)
+void	move_array(int *tab, int first_num, int num_pos)
 {
-	int i;
-	int j;
-	int min;
+	int	i;
 
-	i=0;
-	while (i < size)
+	i = 0;
+	while (tab[i] != first_num)
+	{
+		i++;
+	}
+	while (i > 0)
+	{
+		tab[i] = tab[i - 1];
+		i--;
+	}
+	tab[num_pos] = first_num;
+}
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int	min;
+
+	i = 0;
+	while (i < size -1)
 	{
 		j = 0;
-		min = tab[0];
-		while (j < size -i)
+		min = tab[i];
+		while (j < size - i)
 		{
 			if (tab[i] < min)
 			{
@@ -34,17 +49,21 @@ void ft_sort_int_tab(int *tab, int size)
 			}
 			j++;
 		}
-		
-		size
+		move_array(tab, min, i);
 		i++;
-	}i
+	}
 }
 
 int	main(void)
 {
-	int size = 4;
-	int tab[4] = {0, 6, 3, 6};
-
+	int size = 6;
+	int tab[6] = {0, 6, 3, 6, 7, 1};
 	ft_sort_int_tab(tab, size);
+
+	while (size > 0)
+	{
+		printf("%d ", tab[size]);
+		size--;
+	}
 	return (0);
 }
