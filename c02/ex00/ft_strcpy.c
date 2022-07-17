1a78879b4c8605f2730cf7f39c929ec1763709f0
magnitopic <magnitopic@gmail.com>
2022-07-17 19:10:31 +0200

    finished c02 ex00

---
+++ c02/ex00/ft_strcpy.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alaparic <alaparic@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/17 11:43:44 by alaparic          #+#    #+#             */
+/*   Updated: 2022/07/17 19:07:47 by alaparic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/*int	main(void)
+{
+	char	dest[20];
+	char	*src="Hello There";
+
+	ft_strcpy(dest, src);
+	printf("%s", dest);
+	return (0);
+}*/
