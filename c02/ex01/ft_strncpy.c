b0afea195fdec8ff5920fdb48f3b9251fe73ef8c
magnitopic <magnitopic@gmail.com>
2022-07-17 20:02:03 +0200

    finished ex01

---
+++ c02/ex01/ft_strncpy.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alaparic <alaparic@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/17 19:18:51 by alaparic          #+#    #+#             */
+/*   Updated: 2022/07/17 19:49:34 by alaparic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/*int	main(void)
+{
+	int	n = 5;
+	char			dest[20];
+	char			*src = "Hello There";
+
+	char *output=ft_strncpy(dest, src, n);
+	printf("%s", output);
+	return (0);
+}*/
