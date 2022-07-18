589cda7f7806dddb6cdaf2e7e49bc9950601a53d
magnitopic <magnitopic@gmail.com>
2022-07-18 16:23:11 +0200

    finished ex07

--- c02/ex07/ft_strupcase.c
+++ c02/ex07/ft_strupcase.c
@@ -1,11 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strupcase.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alaparic <alaparic@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/18 16:18:12 by alaparic          #+#    #+#             */
+/*   Updated: 2022/07/18 16:23:04 by alaparic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 
 char *ft_strupcase(char *str)
 {
-	int i;
+	int i=0;
+
 	while (str[i] != '\0')
 	{
-		if (!(str[i] >= 97 && str[i] <= 122))
+		if ((str[i] >= 97 && str[i] <= 122))
 		{
 			str[i] = str[i] - 32;
 		}
@@ -16,7 +29,7 @@ char *ft_strupcase(char *str)
 
 int main(void)
 {
-	char str[12] = "Hello There";
+	char	str[12] = "Hello There";
 	printf("%s", ft_strupcase(str));
 	return (0);
 }
