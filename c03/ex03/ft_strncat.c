7cedbaff334d2d62da38264ce0f42d6306cec294
magnitopic <magnitopic@gmail.com>
2022-07-20 16:42:59 +0200

    finished ex03

---
+++ c03/ex03/ft_strncat.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alaparic <alaparic@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/20 16:28:39 by alaparic          #+#    #+#             */
+/*   Updated: 2022/07/20 16:42:12 by alaparic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0' && j < nb)
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i + 1] = '\0';
+	return (dest);
+}
+
+/*int	main(void)
+{
+	char	src[20] = "There";
+	char	dest[20] = "Hello ";
+	ft_strncat(dest, src, 3);
+	printf("%s", dest);
+	return (0);
+}*/
