9c6f6b5c2b5d9470d3f782d12acd2ecce4f1db5c
magnitopic <magnitopic@gmail.com>
2022-07-20 14:57:30 +0200

    finished ex01

---
+++ c03/ex01/ft_strncmp.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alaparic <alaparic@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/20 13:45:44 by alaparic          #+#    #+#             */
+/*   Updated: 2022/07/20 14:57:01 by alaparic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (!(*s1 == *s2))
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
+		i++;
+	}
+	return (0);
+}
+
+/*int	main(void)
+{
+	char	s1[100]= "Hello There";
+	char	s2[100]= "Hello There";
+
+	printf("%d", ft_strncmp(s1, s2, 5));
+	return (0);
+}*/
