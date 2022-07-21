077ba340eea5d912b6c6564ee30b7542f506fbbd
alaparic <alaparic@student.42madrid.com>
2022-07-21 10:49:47 +0200

    finished ex04

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -6,33 +6,48 @@
 /*   By: alaparic <alaparic@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 16:44:07 by alaparic          #+#    #+#             */
-/*   Updated: 2022/07/20 16:44:15 by alaparic         ###   ########.fr       */
+/*   Updated: 2022/07/21 10:47:41 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+//#include <stdio.h>
+
+int	compare_str(char *str, char *to_find)
+{
+	int	i;
+
+	i = 0;
+	while (to_find[i] != '\0')
+	{
+		if (str[i] != to_find[i])
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
 
 char	*ft_strstr(char *str, char *to_find)
 {
-	int	i;
-	int	j;
-
-	i = 0;
-	j = 0;
-	while (to_find[i] != '\0')
-		i++;
 	while (*str != '\0')
 	{
-		if (*str == )
+		if (*str == *to_find)
+		{
+			if (compare_str(str, to_find))
+			{
+				return (to_find);
+			}
+		}
 		str++;
 	}
-	return ('\0');
+	return ("\0");
 }
 
-int	main(void)
+/*int	main(void)
 {
 	char	str[20] = "Magnitopic";
-	char	to_find[10] = "Mag";
+	char	to_find[20] = "Mag";
 	printf("%s", ft_strstr(str, to_find));
 	return (0);
-}
+}*/
