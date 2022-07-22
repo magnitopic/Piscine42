aeab93b4a0b85d83f2eee774326e529e3f04a30b
magnitopic <magnitopic@gmail.com>
2022-07-22 09:21:29 +0200

    change of idea on ex03 c06

--- c06/ex03/ft_sort_params.c
+++ c06/ex03/ft_sort_params.c
@@ -12,17 +12,36 @@
 
 #include <unistd.h>
 
-void	ft_sort_int_tab(char *tab)
+void print_params(int argc, char *argv[])
+{
+	int	i;
+	int	j;
+
+	j = 1;
+	while (j < argc)
+	{
+		i = 0;
+		while (argv[j][i] != '\0')
+		{
+			write(1, &argv[j][i], 1);
+			i++;
+		}
+		write(1, "\n", 1);
+		j++;
+	}
+	return (0);
+}
+
+
+
+
+int	main(int argc, char *argv[])
 {
 	int	aux;
 	int	pos;
-	int	i;
 
 	pos = 0;
-	i = 0;
-	while (tab[i] != '\0')
-		i++;
-	while (pos < i - 1)
+	while (pos <  - 1)
 	{
 		if (tab[pos] > tab[pos + 1])
 		{
@@ -34,27 +53,6 @@ void	ft_sort_int_tab(char *tab)
 		else
 			pos++;
 	}
-}
-
-int	main(int argc, char *argv[])
-{
-	int	i;
-	int	j;
-
-	argv++;
-	ft_sort_int_tab(argv);
-
-	j = 1;
-	while (argv[j] != '\0')
-	{
-		i = 0;
-		while (argv[j][i] != '\0')
-		{
-			write(1, &argv[j][i], 1);
-			i++;
-		}
-		write(1, "\n", 1);
-		j++;
-	}
+	print_params(argc, argv);
 	return (0);
 }
