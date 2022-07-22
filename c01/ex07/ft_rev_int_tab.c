0a499064890ce3c16d2883d6f0abfd20faf783bd
magnitopic <magnitopic@gmail.com>
2022-07-22 16:56:26 +0200

    fixed ex07 c01

--- c01/ex07/ft_rev_int_tab.c
+++ c01/ex07/ft_rev_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: alaparic <alaparic@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 11:04:59 by alaparic          #+#    #+#             */
-/*   Updated: 2022/07/15 12:43:07 by alaparic         ###   ########.fr       */
+/*   Updated: 2022/07/22 16:55:40 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	ft_rev_int_tab(int *tab, int size)
 	int	aux;
 
 	i = 0;
-	while (i < size - 1)
+	while (i < size / 2)
 	{
 		aux = tab[i];
 		tab[i] = tab[size - 1 - i];
@@ -30,8 +30,8 @@ void	ft_rev_int_tab(int *tab, int size)
 /*int	main(void)
 {
 	int	i = 0;
-	int	size = 5;
-	int	tab[] = {0, 1, 1, 6, 8};
+	int	size = 4;
+	int	tab[] = {0, 1, 1, 6};
 
 	ft_rev_int_tab(tab, size);
 	while (i <= size - 1)
