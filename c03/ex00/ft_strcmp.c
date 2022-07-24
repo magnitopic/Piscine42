caed4e23ef5247b77fff2ab20fa49e726e502ef3
magnitopic <magnitopic@gmail.com>
2022-07-24 18:49:55 +0200

    reduced ex00 c03 code

--- c03/ex00/ft_strcmp.c
+++ c03/ex00/ft_strcmp.c
@@ -11,10 +11,11 @@
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <string.h>
 
 int	ft_strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+	while (*s1 && *s2 && *s1 == *s2)
 	{
 		s1++;
 		s2++;
@@ -24,9 +25,10 @@ int	ft_strcmp(char *s1, char *s2)
 
 /*int	main(void)
 {
-	char	s1[100]= "Hello There";
-	char	s2[100]= "Hello There";
+	char	s1[]= "Hello There";
+	char	s2[]= "Hello There";
 
 	printf("%d", ft_strcmp(s1, s2));
+	printf("\n%d", strcmp(s1, s2));
 	return (0);
 }*/
