d56835c9012dc4053034efa813fc639667f6144d
magnitopic <magnitopic@gmail.com>
2022-07-26 14:17:26 +0200

    finished ex02 c04

--- c04/ex02/ft_putnbr.c
+++ c04/ex02/ft_putnbr.c
@@ -14,21 +14,36 @@
 #include <stdio.h>
 #include <limits.h>
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
 void	ft_putnbr(int nb)
 {
-	char	num;
-
-	while (nb > 0)
+	if (nb == -2147483648)
 	{
-		num = (nb % 10) + '0';
-		write(1, &num, 1);
-		nb = (nb / 10);
+		ft_putchar('-');
+		ft_putchar('2');
+		nb = 147483648;
 	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb *= -1;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	if (nb < 10)
+		ft_putchar(nb + 48);
 }
 
-int main(void)
+/*int	main(void)
 {
 	ft_putnbr(INT_MIN);
-	printf("\n%i", INT_MAX);
+	printf("\n%i", INT_MIN);
 	return (0);
-}
+}*/
